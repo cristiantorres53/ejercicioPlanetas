66d89337500794e71f1a12ccdd49a512a1784f8d
Cristian Camilo Torres Rodriguez <47953810+cristiantorres53@users.noreply.github.com>
2022-03-01 10:09:49 -0500

    modificacion js

---
+++ script/script.js
@@ -0,0 +1,17 @@
+let nombrePlanetas = ["tierra", "marte", "jupiter"];
+let distancia = [6,3, "67"]
+const tamaño = []
+
+for (let i = 0; i < nombrePlanetas.length; i++) {
+    const planeta = nombrePlanetas[i];
+    console.log(nombrePlanetas[i]);
+}
+
+distancia.forEach(key => {
+    if(key == '5'){
+        console.log("se encontro la distancia")
+    }
+    else{
+        console.log("no se encontro la distancia")
+    }
+});
